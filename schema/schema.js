6ceb14b09a4678354a98798b30d838f7200c1143
nikhilnair23 <nikhilnair238@gmail.com>
2020-10-11 14:42:06 -0400

    Added dummy data

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+
+//dummy data
+
+var books = [
+    {name: 'Book A', genre: 'Fantasy', id: '1'},
+    {name: 'Empirical book', genre: 'Fantasy', id: '2'},
+    {name: 'Not a book', genre: 'Sci-Fi', id: '3'}
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -18,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {id: {type: GraphQLString}},
             resolve(parent, args){
+                _.find(books, {id: args.id});
             }
         }
     }
