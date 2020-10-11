0732ac075b38ef54933700a98746869ef5e11d5e
nikhilnair23 <nikhilnair238@gmail.com>
2020-10-11 18:08:19 -0400

    Connected to mongodb atlas

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require("mongoose");
 
 const app = express();
+
+
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.co7ar.mongodb.net/graphql-test?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
